77a0cb7c0a6ec6e4c4a0c6e46f01650d99126682
Derek <d.frand@yahoo.com>
2018-04-13 22:09:41 -0500

    fixed link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = props => (
     <div className="footer">
         <p>
-            <a href="https://github.com/Dfrand/FriendFinder">GitHub Repo</a> | &copy;Copyright 2018 |&nbsp; 
+            <a href="https://github.com/Dfrand/ClickyMcGee">GitHub Repo</a> | &copy;Copyright 2018 |&nbsp; 
             <a href="https://www.linkedin.com/in/derek-frand/">Derek Frand</a>
         </p>
     </div>
