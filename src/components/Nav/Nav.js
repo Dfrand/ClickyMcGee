0a5bbe8a11d4f1aecf6c677eca7e1485363cffbc
Derek <d.frand@yahoo.com>
2018-04-13 21:54:24 -0500

    styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,8 @@ import "./Nav.css";
 const Nav = props => (
     <nav className="navbar navbar-default">
             <div className="brand">Clicky McGee</div>
-            <p className="navbar-text scores">Current Score: {props.score} <br/>
-            High Score: {props.highScore} </p>
+            <div className="navbar-text scores">Current Score: {props.score} <br/>
+            High Score: {props.highScore} </div>
     </nav>
 );
 
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./Wrapper.css";
 
 const   Wrapper = props => (
     <div className="row">
         <div className="wrapper">{props.children}</div>
+        <div className="Why">What are you doing down here?! Go back up there and beat the High Score!</div>
     </div>
 );
 
